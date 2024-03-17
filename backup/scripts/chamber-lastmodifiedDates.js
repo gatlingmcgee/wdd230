e4df5169bf73f5a4b9b4aa0bd1acefabac6e5ac7
gatlingmcgee <rzman03@aol.com>
2024-03-16 23:23:24 -0500

    week03discoverupdate2

--- backup/scripts/chamber-lastmodifiedDates.js
+++ backup/scripts/chamber-lastmodifiedDates.js
@@ -4,6 +4,36 @@ currentYearElement.textContent = new Date().getFullYear();
 const lastModifiedElement = document.getElementById('lastModified');
 lastModifiedElement.textContent = document.lastModified;
 
+/* ---------------------------First Visit Message----------------------------- */
+
+const visitInformation = document.querySelector(".visits");
+
+let numberVisits = Number(window.localStorage.getItem("numberVisits-ls")) || 0;
+
+if (numberVisits !== 0) {
+	visitInformation.textContent = numberVisits;
+} else {
+	visitInformation.textContent = `\nWelcome! Let us know if you have any questions.`;
+}
+
+numberVisits++;
+
+localStorage.setItem("numberVisits-ls", numberVisits);
+
+/* ---------------------------Last Visit Date----------------------------- */
+
+const lastVisitDate = localStorage.getItem("lastVisitDate");
+
+if (lastVisitDate) {
+    const timeDiff = Date.now() - new Date(lastVisitDate);
+    const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+    const welcomeMessage = `You last visited ${daysDiff} days ago.`;
+    document.querySelector(".message").textContent = welcomeMessage;
+}
+
+localStorage.setItem("lastVisitDate", new Date().toISOString());
+
 /* ---------------------------Hamburger----------------------------- */
 
 const button = document.querySelector('#hamburger-menu');
@@ -13,3 +43,11 @@ button.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	button.classList.toggle('open');
 });
+
+/* ---------------------------Go Back----------------------------- */
+
+function goBack() {
+	window.history.back();
+}
+
+
--- chamber/scripts/lastmodifiedDates.js
+++ chamber/scripts/lastmodifiedDates.js
@@ -4,7 +4,7 @@ currentYearElement.textContent = new Date().getFullYear();
 const lastModifiedElement = document.getElementById('lastModified');
 lastModifiedElement.textContent = document.lastModified;
 
-/* ---------------------------Last Visit Message----------------------------- */
+/* ---------------------------First Visit Message----------------------------- */
 
 const visitInformation = document.querySelector(".visits");
 
@@ -13,12 +13,26 @@ let numberVisits = Number(window.localStorage.getItem("numberVisits-ls")) || 0;
 if (numberVisits !== 0) {
 	visitInformation.textContent = numberVisits;
 } else {
-	visitInformation.textContent = `\nYou're the first visitor. Hello!`;
+	visitInformation.textContent = `\nWelcome! Let us know if you have any questions.`;
 }
 
 numberVisits++;
 
 localStorage.setItem("numberVisits-ls", numberVisits);
+
+/* ---------------------------Last Visit Date----------------------------- */
+
+const lastVisitDate = localStorage.getItem("lastVisitDate");
+
+if (lastVisitDate) {
+    const timeDiff = Date.now() - new Date(lastVisitDate);
+    const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+    const welcomeMessage = `You last visited ${daysDiff} days ago.`;
+    document.querySelector(".message").textContent = welcomeMessage;
+}
+
+localStorage.setItem("lastVisitDate", new Date().toISOString());
 
 /* ---------------------------Hamburger----------------------------- */
 

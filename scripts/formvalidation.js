2544f779f627e60ed18f68afaecc1b98176299cc
gatlingmcgee <rzman03@aol.com>
2024-03-19 20:38:31 -0500

    week04formassignment

---
+++ scripts/formvalidation.js
@@ -0,0 +1,32 @@
+const password = document.querySelector("#password");
+const confirm = document.querySelector("#confirm");
+const pfeedback = document.querySelector("#pfeedback");
+
+confirm.addEventListener('focusout', controlar)
+
+function controlar() {
+    //console.log('inside the function')
+    if (password.value !== confirm.value) {
+        //console.log('no match')
+        password.value = ""
+        confirm.value = ""
+        password.focus()
+        pfeedback.textContent = "Values do not match. Please try again."
+
+    } else {
+            //console.log('it is a match')
+            pfeedback.textContent = ""
+        }
+    }
+
+
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("range");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
